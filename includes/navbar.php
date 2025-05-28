847b40f3180b7b8bb66733cbd33ec9e165b45858
gcrenovich <gcrenovich@gmail.com>
2025-05-28 11:48:43 -0300

    65465

---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="dashboard.php">Kanban</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="dashboard.php">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="abm_usuarios.php">Usuarios</a></li>
+        <li class="nav-item"><a class="nav-link" href="abm_sectores.php">Sectores</a></li>
+        <li class="nav-item"><a class="nav-link" href="abm_tareas.php">Tareas</a></li>
+        <li class="nav-item"><a class="nav-link" href="logout.php">Salir</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
