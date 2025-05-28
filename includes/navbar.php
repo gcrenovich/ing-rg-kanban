4eb4c4322701aaf2df595c4075ec85b24cf84b3c
julianzayas <julianzayas88@gmail.com>
2025-05-28 17:19:24 -0300

    mod-cambios

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
   <div class="container-fluid">
     <a class="navbar-brand" href="dashboard.php">Kanban</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -11,6 +11,26 @@
         <li class="nav-item"><a class="nav-link" href="abm_sectores.php">Sectores</a></li>
         <li class="nav-item"><a class="nav-link" href="abm_tareas.php">Tareas</a></li>
         <li class="nav-item"><a class="nav-link" href="logout.php">Salir</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+ -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm mb-4">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="dashboard.php">
+      <i class="bi bi-kanban"></i> Kanban
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link active" aria-current="page" href="dashboard.php">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="abm_usuarios.php">Usuarios</a></li>
+        <li class="nav-item"><a class="nav-link" href="abm_sectores.php">Sectores</a></li>
+        <li class="nav-item"><a class="nav-link" href="abm_tareas.php">Tareas</a></li>
+        <li class="nav-item"><a class="nav-link text-danger" href="logout.php">Salir</a></li>
       </ul>
     </div>
   </div>
